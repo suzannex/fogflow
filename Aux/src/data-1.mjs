361611a1948a6c6681bc28796274b5f775d6b13e
suzannex <becker.su@husky.neu.edu>
2018-10-02 22:14:31 -0400

    fix bugs in drawing file

---
+++ Aux/src/data-1.mjs
@@ -0,0 +1,937 @@
+export const location = "FOGxIsland"
+export const time = "6:08";
+export const ampm = "PM";
+// Created by running dataParser.js on CSV input from stdin
+export const data = [
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "0",
+    "departure": "2"
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "3",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "4",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "4",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "5",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "6",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "8",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "10",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "10",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "10",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "11",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "up",
+    "arrival": "11",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "12",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "12",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "12",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "12",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "12",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "12",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "13",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "13",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "up",
+    "arrival": "13",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "up",
+    "arrival": "13",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "up",
+    "arrival": "13",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "up",
+    "arrival": "13",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "13",
+    "departure": "17"
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "13",
+    "departure": "17"
+  },
+  {
+    "type": "r",
+    "dir": "up",
+    "arrival": "14",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "14",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "14",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "14",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "14",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "up",
+    "arrival": "15",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "up",
+    "arrival": "15",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "16",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "16",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "16",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "up",
+    "arrival": "16",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "16",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "16",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "16",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "16",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "up",
+    "arrival": "17",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "17",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "17",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "17",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "18",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "19",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "20",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "20",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "20",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "20",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "20",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "20",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "21",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "21",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "21",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "21",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "22",
+    "departure": "29"
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "22",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "22",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "22",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "23",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "23",
+    "departure": "24"
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "24",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "24",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "24",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "24",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "25",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "25",
+    "departure": "27"
+  },
+  {
+    "type": "c",
+    "dir": "up",
+    "arrival": "26",
+    "departure": "27"
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "27",
+    "departure": "28"
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "27",
+    "departure": "28"
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "28",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "28",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "28",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "28",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "28",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "28",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "28",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "up",
+    "arrival": "28",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "28",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "28",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "28",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "29",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "29",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "29",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "up",
+    "arrival": "29",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "30",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "30",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "31",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "32",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "33",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "33",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "33",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "33",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "34",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "34",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "34",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "34",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "34",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "35",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "35",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "35",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "up",
+    "arrival": "36",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "37",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "37",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "37",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "37",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "38",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "38",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "38",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "38",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "38",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "38",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "38",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "38",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "39",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "39",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "40",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "40",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "42",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "43",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "43",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "43",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "up",
+    "arrival": "43",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "up",
+    "arrival": "43",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "44",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "45",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "45",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "45",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "45",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "46",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "46",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "46",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "46",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "47",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "down",
+    "arrival": "48",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "48",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "up",
+    "arrival": "49",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "50",
+    "departure": "52"
+  },
+  {
+    "type": "c",
+    "dir": "up",
+    "arrival": "52",
+    "departure": "57"
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "52",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "53",
+    "departure": "57"
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "53",
+    "departure": "57"
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "53",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "up",
+    "arrival": "54",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "up",
+    "arrival": "54",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "54",
+    "departure": null
+  },
+  {
+    "type": "c",
+    "dir": "down",
+    "arrival": "54",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "55",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "down",
+    "arrival": "56",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "56",
+    "departure": null
+  },
+  {
+    "type": "r",
+    "dir": "up",
+    "arrival": "57",
+    "departure": null
+  },
+  {
+    "type": "w",
+    "dir": "up",
+    "arrival": "57",
+    "departure": null
+  }
+];
+
+// module.exports = {
+//   "location" : location,
+//   "time" : time,
+//   "ampm" : ampm,
+//   "visitData" : data
+// };
